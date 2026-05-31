855d1f25ad1ed313c2177a3d27571baac9fa3189
esther <totok0818@gmail.com>
2026-05-31 21:27:30 +0900

    feat: 마이페이지 내 정보 조회 API구현

--- src/routes/user.js
+++ src/routes/user.js
@@ -7,6 +7,7 @@ const passport = require("passport");
 
 const isLogin = passport.authenticate("jwt", { session: false });
 
+router.get("/me", isLogin, userController.getMe);
 router.get("/search", isLogin, userController.searchUserByCode);
 router.get("/:userId/profile", isLogin, userController.getUserProfile);
 
